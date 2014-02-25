4d221077ba38a4d37dda67e28fb469512d5caa9a
Vsevolod Romashov <7@7vn.ru>
2014-02-26 00:43:58 +0400

    lol

--- cookbooks/web/attributes/default.rb
+++ cookbooks/web/attributes/default.rb
@@ -1,23 +1,14 @@
-default.rbenv = {
-  user_installs: [
-    {
-      user: 'web',
-      home: '/home/web',
-      git_url: 'git://github.com/sstephenson/rbenv.git',
-      git_ref: 'v0.4.0',
-      rubies: %w(2.0.0-p451),
-      global: '2.0.0-p451',
-      gems: {
-        '2.0.0-p451' => [
-          { name: 'bundler' }
-        ]
-      }
+default.rbenv.user_installs = [
+  {
+    user: 'web',
+    rubies: %w(2.0.0-p451),
+    global: '2.0.0-p451',
+    gems: {
+      '2.0.0-p451' => [
+        { name: 'bundler' }
+      ]
     }
-  ]
-}
+  }
+]
 
-default.ruby_build = {
-  upgrade: true,
-  git_url: 'git://github.com/sstephenson/ruby-build.git',
-  git_ref: 'master'
-}
+default.ruby_build.upgrade = true
