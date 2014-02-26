78e8d97711a63d4cf39a556094a367b3b157f5f6
Vsevolod Romashov <7@7vn.ru>
2014-02-27 01:21:17 +0400

    sudo & /etc/sudoers

--- cookbooks/common/attributes/default.rb
+++ cookbooks/common/attributes/default.rb
@@ -2,3 +2,6 @@ default.localegen.lang = [
   'en_US.UTF-8 UTF-8',
   'ru_RU.UTF-8 UTF-8'
 ]
+
+default.authorization.sudo.users = %w(web)
+default.authorization.sudo.groups = %w(sudo)
--- cookbooks/common/recipes/default.rb
+++ cookbooks/common/recipes/default.rb
@@ -12,3 +12,5 @@ include_recipe 'locale-gen'
 cookbook_file 'sources.list' do
   path '/etc/apt/sources.list'
 end
+
+include_recipe 'sudo'
